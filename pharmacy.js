857f430612e07c2884829ddfb7c991501158f626
Julien Robberechts <robberechts.consulting@gmail.com>
2026-07-23 12:00:21 +0200

    refactor: declarative expiry deltas

--- pharmacy.js
+++ pharmacy.js
@@ -11,21 +11,37 @@ export const DRUG_NAMES = {
   DAFALGAN: "Dafalgan",
 };
 
-const DEFAULT_RULE = { getBenefitDelta: () => -1, expiresInDelta: -1 };
+const DEFAULT_RULE = {
+  benefitDeltaBeforeExpiry: -1, // benefit change per day before expiration
+  benefitDeltaAfterExpiry: -2, // benefit change per day once expired
+  expiresInDeltaPerDay: -1, // expiresIn change per day
+};
 
 const DRUG_RULES = {
-  [DRUG_NAMES.HERBAL_TEA]: { getBenefitDelta: () => 1, expiresInDelta: -1 },
+  [DRUG_NAMES.HERBAL_TEA]: {
+    benefitDeltaBeforeExpiry: 1,
+    benefitDeltaAfterExpiry: 2,
+    expiresInDeltaPerDay: -1,
+  },
   [DRUG_NAMES.FERVEX]: {
-    getBenefitDelta: (expiresIn) => {
+    benefitDeltaBeforeExpiry: (expiresIn) => {
       if (expiresIn <= 5) return 3;
       if (expiresIn <= 10) return 2;
       return 1;
     },
-    expiresInDelta: -1,
-    onExpired: () => 0,
+    benefitValueAfterExpiry: 0,
+    expiresInDeltaPerDay: -1,
   },
-  [DRUG_NAMES.MAGIC_PILL]: { getBenefitDelta: () => 0, expiresInDelta: 0 },
-  [DRUG_NAMES.DAFALGAN]: { getBenefitDelta: () => -2, expiresInDelta: -1 },
+  [DRUG_NAMES.MAGIC_PILL]: {
+    benefitDeltaBeforeExpiry: 0,
+    benefitDeltaAfterExpiry: 0,
+    expiresInDeltaPerDay: 0,
+  },
+  [DRUG_NAMES.DAFALGAN]: {
+    benefitDeltaBeforeExpiry: -2,
+    benefitDeltaAfterExpiry: -4,
+    expiresInDeltaPerDay: -1,
+  },
 };
 
 export class Drug {
@@ -37,15 +53,23 @@ export class Drug {
 
   update() {
     const rule = DRUG_RULES[this.name] || DEFAULT_RULE;
-    const benefitDelta = rule.getBenefitDelta(this.expiresIn);
-    this.benefit = clampBenefit(this.benefit + benefitDelta);
-    this.expiresIn += rule.expiresInDelta;
+    const benefitDeltaBeforeExpiry =
+      typeof rule.benefitDeltaBeforeExpiry === "function"
+        ? rule.benefitDeltaBeforeExpiry(this.expiresIn)
+        : rule.benefitDeltaBeforeExpiry;
 
-    if (this.expiresIn < 0) {
-      this.benefit = rule.onExpired
-        ? rule.onExpired(this.benefit)
-        : clampBenefit(this.benefit + benefitDelta);
-    }
+    this.expiresIn += rule.expiresInDeltaPerDay;
+    const expired = this.expiresIn < 0;
+
+    this.benefit =
+      expired && rule.benefitValueAfterExpiry !== undefined
+        ? clampBenefit(rule.benefitValueAfterExpiry)
+        : clampBenefit(
+            this.benefit +
+              (expired
+                ? rule.benefitDeltaAfterExpiry
+                : benefitDeltaBeforeExpiry),
+          );
   }
 }
 
